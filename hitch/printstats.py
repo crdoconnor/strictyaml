93ef86f9fa086cad1a78384a423132c1508c0ea0
Colm O'Connor <colm.oconnor.github@gmail.com>
2017-10-06 00:53:43 +0100

    REFACTOR : Do not break printstats.

--- hitch/printstats.py
+++ hitch/printstats.py
@@ -1,6 +1,8 @@
 import pstats
 import sys
+import os
 
-p = pstats.Stats(sys.argv[1])
-p.sort_stats("cumulative")
-p.print_stats()
+if os.path.exists(sys.argv[1]):
+    p = pstats.Stats(sys.argv[1])
+    p.sort_stats("cumulative")
+    p.print_stats()
