c8ac37d66a901a245286b992a4c256362a27b38a
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-02-23 20:07:33 +0000

    TOOLING : Refactored run.sh a bit.

--- key.sh
+++ key.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 set -e
+PROJECT_NAME=strictyaml
+
 PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+CONTAINER_NAME=${PROJECT_NAME}-hitch-container
+IMAGE_NAME=${PROJECT_NAME}-hitch
 
 hitchrun() {
     podman run --privileged -it --rm \
         -v $PROJECT_DIR:/src \
-        -v strictyaml-hitch-container:/gen \
+        -v $CONTAINER_NAME:/gen \
         -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
         -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
         --workdir /src \
-        strictyaml-hitch \
+        $IMAGE_NAME \
         $1
 }
 
 
 case "$1" in
     "clean")
-        if podman volume exists strictyaml-hitch-container; then
-            podman volume rm strictyaml-hitch-container
+        if podman volume exists $CONTAINER_NAME; then
+            podman volume rm $CONTAINER_NAME
         fi
-        if podman image exists strictyaml-hitch; then
-            podman image rm -f strictyaml-hitch
+        if podman image exists $IMAGE_NAME; then
+            podman image rm -f $IMAGE_NAME
         fi
         ;;
     "make")
         echo "building ci container..."
-        if ! podman volume exists strictyaml-hitch-container; then
-            podman volume create strictyaml-hitch-container
+        if ! podman volume exists $CONTAINER_NAME; then
+            podman volume create $CONTAINER_NAME
         fi
-        podman build -f hitch/Dockerfile-hitch -t strictyaml-hitch $PROJECT_DIR
+        podman build -f hitch/Dockerfile-hitch -t $CONTAINER_NAME $PROJECT_DIR
         hitchrun "/venv/bin/python hitch/key.py make"
         ;;
     "bash")
         hitchrun "bash"
-        ;;
-    "--help")
-        echo "Commands:"
-        echo "./run.sh make     - build docker containers."
         ;;
     *)
         hitchrun "/venv/bin/python hitch/key.py $1 $2 $3 $4 $5 $6 $7 $8 $9"
